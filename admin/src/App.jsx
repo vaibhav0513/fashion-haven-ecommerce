5bcb722894a84dd5aa3ef21063bd106ca064fcba
Vaibhav Tupe <141541491+vaibhav0513@users.noreply.github.com>
2024-10-11 12:08:15 +0530

    feature

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,6 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
 export const currency = '$'
 
+
 const App = () => {
   const [token, setToken] =  useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
 
