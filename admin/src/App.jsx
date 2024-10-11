395bbe4aa9778b408f60ca2f7f3342ec885ecc7b
Vaibhav Tupe <141541491+vaibhav0513@users.noreply.github.com>
2024-10-11 11:58:18 +0530

    bugs fix

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Routes } from 'react-router-dom'
 import Add from './pages/Add'
 import Orders from './pages/Orders'
 import List from './pages/List'
